dfb69437af622a865a632b86f2bfb7fc12fa1e43
Ryan-Jing <ryanjing23@gmail.com>
2023-05-27 20:56:50 -0400

    File organization

--- CovidCaseProgram.py
+++ CovidCaseProgram.py
@@ -7,18 +7,18 @@ import pandas as pd
 url = 'https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/ed270bb8-340b-41f9-a7c6-e8ef587e6d11/download/covidtesting.csv'
 r = requests.get(url, allow_redirects=True)
 
-open('Covid Data.csv', 'wb').write(r.content)
+open('Covid_Data.csv', 'wb').write(r.content)
 
 
 col = [0,6,7]
 
-with open("Covid Data.csv") as fr, open("Covid Data Updated.csv","w",newline="") as fw:
+with open("Covid_Data.csv") as fr, open("Covid_Data_Updated.csv", "w", newline="") as fw:
     cr = csv.reader(fr)
     cw = csv.writer(fw)
     cw.writerows([row[i] for i in col] for row in cr)
 
 gc = pygsheets.authorize(service_file='./covid-cases-project-new-83175c29a1cb.json')
-df = pd.read_csv('Covid Data.csv', usecols = ['Reported Date','Confirmed Positive','Deaths'], low_memory = True)
+df = pd.read_csv('Covid_Data.csv', usecols = ['Reported Date', 'Confirmed Positive', 'Deaths'], low_memory = True)
 sh = gc.open('CovidCasesData')
 wks = sh[0]
 wks.set_dataframe(df,(1,1))
